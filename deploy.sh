5a53337c7c3cc6aaa51d371dafa4e47c779a3dd0
VanLiuZhi <1441765847@qq.com>
2022-03-06 10:12:31 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # 设置目标OS环境变量，编译代码，并移动到虚拟机共享目录
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main main.go
-mv ./main /Users/liuzhi/VagrantFile/centos
+#mv ./main /Users/liuzhi/VagrantFile/centos
